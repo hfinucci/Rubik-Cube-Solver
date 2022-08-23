a250dfe7ee68404fba282aa1198a7f3c23b61938
Felipe <65612488+felu98@users.noreply.github.com>
2022-08-22 22:38:59 -0300

    MixUPCube

--- cube.py
+++ cube.py
@@ -1,65 +1,64 @@
 import random
 import numpy as np
-
 from constants import *
+
 
 class Cube:
     def __init__(self, n):
-        self.cube = np.zeros((6, n, n), dtype=int)
+        self.cube = np.zeros((6, n, n), dtype=int)  # numpy.int8
+        self.n = n
 
-        for i in range(0,6):
+        for i in range(0, 6):
             self.cube[i] = i
-
 
     # Rotates faces 0->1->2->3->0 if RIGHT
     def RotateXAxis(self, row, direction):
-        if (direction == RIGHT):
+        if direction == RIGHT:
             aux = np.array(self.cube[3][row, :])
             self.cube[3][row, :] = self.cube[2][row, :]
             self.cube[2][row, :] = self.cube[1][row, :]
-            self.cube[1][row, :] = self.cube[0][row, :]        
+            self.cube[1][row, :] = self.cube[0][row, :]
             self.cube[0][row, :] = aux
-        elif (direction == LEFT):
+        elif direction == LEFT:
             aux = np.array(self.cube[0][row, :])
             self.cube[0][row, :] = self.cube[1][row, :]
             self.cube[1][row, :] = self.cube[2][row, :]
-            self.cube[2][row, :] = self.cube[3][row, :]        
+            self.cube[2][row, :] = self.cube[3][row, :]
             self.cube[3][row, :] = aux
 
     # Rotates 0->4->2->5->0 if UP
     def RotateYAxis(self, col, direction):
-        if (direction == UP):
+        if direction == UP:
             aux = np.array(self.cube[5][:, col])
             self.cube[5][:, col] = self.cube[2][:, col]
             self.cube[2][:, col] = self.cube[4][:, col]
-            self.cube[4][:, col] = self.cube[0][:, col]        
+            self.cube[4][:, col] = self.cube[0][:, col]
             self.cube[0][:, col] = aux
-        elif (direction == DOWN):
+        elif direction == DOWN:
             aux = np.array(self.cube[0][:, col])
             self.cube[0][:, col] = self.cube[4][:, col]
             self.cube[4][:, col] = self.cube[2][:, col]
-            self.cube[2][:, col] = self.cube[5][:, col]        
+            self.cube[2][:, col] = self.cube[5][:, col]
             self.cube[5][:, col] = aux
 
     def RotateZAxis(self, face, direction):
-        if (direction == CLOCKWISE):
-            self.cube[face] = np.rot90(self.cube[face], axes=(1,0))
-            
-        elif (direction == ANTI_CLOCKWISE):
+        if direction == CLOCKWISE:
+            self.cube[face] = np.rot90(self.cube[face], axes=(1, 0))
+
+        elif direction == ANTI_CLOCKWISE:
             self.cube[face] = np.rot90(self.cube[face])
 
-    def isDone():
+    def isDone(self):
         pass
 
-
-
-
-cube = Cube(3)
-# cube.RotateXAxis(0,RIGHT)
-cube.RotateYAxis(1, DOWN)
-# cube.RotateZAxis(1, CLOCKWISE)
-
-print(cube.cube)
-
-
-
+    def mix_up(self, num):
+        for x in range(num):
+            row = random.randint(0, self.n-1)
+            direction = random.randint(0, 1)
+            match random.randint(0, 2):
+                case 0:
+                    self.RotateXAxis(row, direction)
+                case 1:
+                    self.RotateYAxis(row, direction)
+                case 2:
+                    self.RotateZAxis(row, direction)
--- interface.py
+++ interface.py
@@ -1,6 +1,6 @@
-import sys, pygame
+import pygame
+import sys
 from cube import Cube
-from constants import *
 
 pygame.init()
 
@@ -12,7 +12,7 @@ INITIAL_Y = 20
 RECT_WIDTH = 0
 CELL_WIDTH = 20
 CELL_HEIGHT = 20
-CUBE_WIDTH = 3 * CELL_WIDTH + 3 * MARGIN 
+CUBE_WIDTH = 3 * CELL_WIDTH + 3 * MARGIN
 CUBE_HEIGHT = 3 * CELL_HEIGHT + 3 * MARGIN
 
 BLACK_RGB = 0, 0, 0
@@ -21,71 +21,89 @@ BACKGROUND_COLOR = (225, 225, 225)
 
 def calculate_color(number):
     match number:
-        case 0: # BLUE
-            return (0,0,255)
-        case 1: # RED
-            return (255,0,0)
-        case 2: # GREEN
-            return (60,179,113)
-        case 3: # YELLOW
-            return (255,255,100)
-        case 4: # WHITE
-            return (255,255,255)
-        case 5: # ORANGE
-            return (255,165,0)
-        case _: # DEFAULT RETURNS BLACK
-            return (0,0,0)
+        case 0:  # BLUE
+            return 0, 0, 255
+        case 1:  # RED
+            return 255, 0, 0
+        case 2:  # GREEN
+            return 60, 179, 113
+        case 3:  # YELLOW
+            return 255, 255, 100
+        case 4:  # WHITE
+            return 255, 255, 255
+        case 5:  # ORANGE
+            return 255, 165, 0
+        case _:  # DEFAULT RETURNS BLACK
+            return 0, 0, 0
 
 
-def draw_cube(x, y, cell_width, cell_height, colors):
-    pygame.draw.rect(screen, calculate_color(colors[0]), (x,y,cell_width,cell_height), width=RECT_WIDTH)
-    pygame.draw.rect(screen, calculate_color(colors[1]), (MARGIN+x+cell_width,y,cell_width,cell_height), width=RECT_WIDTH)
-    pygame.draw.rect(screen, calculate_color(colors[2]), (2*MARGIN+x+cell_width*2,y,cell_width,cell_height), width=RECT_WIDTH)
+def _draw_cube(x, y, cell_width, cell_height, colors):
+    pygame.draw.rect(screen, calculate_color(colors[0]), (x, y, cell_width, cell_height), width=RECT_WIDTH)
+    pygame.draw.rect(screen, calculate_color(colors[1]), (MARGIN + x + cell_width, y, cell_width, cell_height),
+                     width=RECT_WIDTH)
+    pygame.draw.rect(screen, calculate_color(colors[2]), (2 * MARGIN + x + cell_width * 2, y, cell_width, cell_height),
+                     width=RECT_WIDTH)
 
-    pygame.draw.rect(screen, calculate_color(colors[3]), (x,MARGIN+y+CELL_HEIGHT,20,20), width=RECT_WIDTH)
-    pygame.draw.rect(screen, calculate_color(colors[4]), (MARGIN+x+cell_width,MARGIN+y+CELL_HEIGHT,cell_width,cell_height), width=RECT_WIDTH)
-    pygame.draw.rect(screen, calculate_color(colors[5]), (2*MARGIN+x+cell_width*2,MARGIN+y+CELL_HEIGHT,cell_width,cell_height), width=RECT_WIDTH)
+    pygame.draw.rect(screen, calculate_color(colors[3]), (x, MARGIN + y + CELL_HEIGHT, 20, 20), width=RECT_WIDTH)
+    pygame.draw.rect(screen, calculate_color(colors[4]),
+                     (MARGIN + x + cell_width, MARGIN + y + CELL_HEIGHT, cell_width, cell_height), width=RECT_WIDTH)
+    pygame.draw.rect(screen, calculate_color(colors[5]),
+                     (2 * MARGIN + x + cell_width * 2, MARGIN + y + CELL_HEIGHT, cell_width, cell_height),
+                     width=RECT_WIDTH)
 
-    pygame.draw.rect(screen, calculate_color(colors[6]), (x,2*MARGIN+y+CELL_HEIGHT*2,cell_width,cell_height), width=RECT_WIDTH)
-    pygame.draw.rect(screen, calculate_color(colors[7]), (MARGIN+x+cell_width,2*MARGIN+y+CELL_HEIGHT*2,cell_width,cell_height), width=RECT_WIDTH)
-    pygame.draw.rect(screen, calculate_color(colors[8]), (2*MARGIN+x+cell_width*2,2*MARGIN+y+CELL_HEIGHT*2,cell_width,cell_height), width=RECT_WIDTH)
+    pygame.draw.rect(screen, calculate_color(colors[6]), (x, 2 * MARGIN + y + CELL_HEIGHT * 2, cell_width, cell_height),
+                     width=RECT_WIDTH)
+    pygame.draw.rect(screen, calculate_color(colors[7]),
+                     (MARGIN + x + cell_width, 2 * MARGIN + y + CELL_HEIGHT * 2, cell_width, cell_height),
+                     width=RECT_WIDTH)
+    pygame.draw.rect(screen, calculate_color(colors[8]),
+                     (2 * MARGIN + x + cell_width * 2, 2 * MARGIN + y + CELL_HEIGHT * 2, cell_width, cell_height),
+                     width=RECT_WIDTH)
+
+
+def draw_cube(my_cube):
+    _draw_cube(INITIAL_X + CUBE_WIDTH, INITIAL_Y, CELL_WIDTH, CELL_HEIGHT, my_cube.cube[2].flatten())
+    _draw_cube(INITIAL_X + CUBE_WIDTH, INITIAL_Y + CUBE_HEIGHT, CELL_WIDTH, CELL_HEIGHT, my_cube.cube[5].flatten())
+    _draw_cube(INITIAL_X + CUBE_WIDTH, INITIAL_Y + CUBE_HEIGHT * 2, CELL_WIDTH, CELL_HEIGHT, my_cube.cube[0].flatten())
+    _draw_cube(INITIAL_X, INITIAL_Y + CUBE_HEIGHT, CELL_WIDTH, CELL_HEIGHT, my_cube.cube[3].flatten())
+    _draw_cube(INITIAL_X + CUBE_WIDTH * 2, INITIAL_Y + CUBE_HEIGHT, CELL_WIDTH, CELL_HEIGHT, my_cube.cube[1].flatten())
+    _draw_cube(INITIAL_X + CUBE_WIDTH * 3, INITIAL_Y + CUBE_HEIGHT, CELL_WIDTH, CELL_HEIGHT, my_cube.cube[4].flatten())
 
 
 screen = pygame.display.set_mode(size)
-
 cube = Cube(3)
-
-# cube.RotateXAxis(0,RIGHT)
-# cube.RotateXAxis(2,LEFT)
-ticks = 2000
 while 1:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT:
+            sys.exit()
 
     screen.fill(BACKGROUND_COLOR)
 
-    draw_cube(INITIAL_X + CUBE_WIDTH, INITIAL_Y, CELL_WIDTH, CELL_HEIGHT, cube.cube[2].flatten())
-    draw_cube(INITIAL_X + CUBE_WIDTH, INITIAL_Y + CUBE_HEIGHT, CELL_WIDTH, CELL_HEIGHT, cube.cube[5].flatten())
-    draw_cube(INITIAL_X + CUBE_WIDTH, INITIAL_Y + CUBE_HEIGHT * 2, CELL_WIDTH, CELL_HEIGHT, cube.cube[0].flatten())
-    draw_cube(INITIAL_X, INITIAL_Y + CUBE_HEIGHT, CELL_WIDTH, CELL_HEIGHT, cube.cube[3].flatten())
-    draw_cube(INITIAL_X + CUBE_WIDTH * 2, INITIAL_Y + CUBE_HEIGHT, CELL_WIDTH, CELL_HEIGHT, cube.cube[1].flatten())
-    draw_cube(INITIAL_X + CUBE_WIDTH * 3, INITIAL_Y + CUBE_HEIGHT, CELL_WIDTH, CELL_HEIGHT, cube.cube[4].flatten())
-
-    pygame.time.wait(100)
-    cube.RotateXAxis(0,RIGHT)
-    pygame.time.wait(100)
-    cube.RotateYAxis(2,UP)
-    pygame.time.wait(100)
-    cube.RotateXAxis(2,LEFT)
-    pygame.time.wait(100)
-    cube.RotateYAxis(1,DOWN)
-    pygame.time.wait(100)
-    cube.RotateZAxis(3,CLOCKWISE)
-    pygame.time.wait(100)
-    cube.RotateYAxis(0,DOWN)
-
+    draw_cube(cube)
+    cube.mix_up(1)
+    pygame.time.wait(300)
     pygame.display.flip()
 
-
+    """"
+    _draw_cube(INITIAL_X + CUBE_WIDTH, INITIAL_Y, CELL_WIDTH, CELL_HEIGHT, cube.cube[2].flatten())
+    _draw_cube(INITIAL_X + CUBE_WIDTH, INITIAL_Y + CUBE_HEIGHT, CELL_WIDTH, CELL_HEIGHT, cube.cube[5].flatten())
+    _draw_cube(INITIAL_X + CUBE_WIDTH, INITIAL_Y + CUBE_HEIGHT * 2, CELL_WIDTH, CELL_HEIGHT, cube.cube[0].flatten())
+    _draw_cube(INITIAL_X, INITIAL_Y + CUBE_HEIGHT, CELL_WIDTH, CELL_HEIGHT, cube.cube[3].flatten())
+    _draw_cube(INITIAL_X + CUBE_WIDTH * 2, INITIAL_Y + CUBE_HEIGHT, CELL_WIDTH, CELL_HEIGHT, cube.cube[1].flatten())
+    _draw_cube(INITIAL_X + CUBE_WIDTH * 3, INITIAL_Y + CUBE_HEIGHT, CELL_WIDTH, CELL_HEIGHT, cube.cube[4].flatten())
+    
+    pygame.time.wait(100)
+    cube.RotateXAxis(0, RIGHT)
+    pygame.time.wait(100)
+    cube.RotateYAxis(2, UP)
+    pygame.time.wait(100)
+    cube.RotateXAxis(2, LEFT)
+    pygame.time.wait(100)
+    cube.RotateYAxis(1, DOWN)
+    pygame.time.wait(100)
+    cube.RotateZAxis(3, CLOCKWISE)
+    pygame.time.wait(100)
+    cube.RotateYAxis(0, DOWN)
+    """
 
 
